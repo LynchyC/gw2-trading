94ec9ff54a6aac4fef6c02c367ec44a02b697b30
git <charlie.lynch95@gmail.com>
2016-03-22 20:57:27 +0000

    Added backend API to retrieve GW2 Item & Price data

---
+++ app/controllers/items.server.controller.js
@@ -0,0 +1,78 @@
+// .app/controllers/items.server.controller
+
+'use strict';
+
+var request = require('request');
+
+exports.getItemData = (req, res) => {
+    let id = req.query.itemID;
+    let itemData = {
+        name: '',
+        img: '',
+        buys: {
+            quantity: null,
+            price: null
+        },
+        sells: {
+            quantity: null,
+            price: null
+        }
+    };
+
+    request(`https://api.guildwars2.com/v2/items/${id}`, (err, response, body) => {
+
+        var resBody = JSON.parse(body);
+
+        if (resBody.text == 'no such id') {
+            res.render('index', {
+                layout: false,
+                title: "Error has occured",
+                error: "No such ID. Please try again"
+            });
+        } else if (!err && res.statusCode == 200) {
+
+            itemData = Object.assign(itemData, {
+                name: resBody.name,
+                img: resBody.icon
+            });
+
+            exports.getPriceData(res, id, itemData);
+        }
+    });
+};
+
+exports.getPriceData = (res, id, itemData) => {
+    request(`https://api.guildwars2.com/v2/commerce/prices/${id}`, (err, response, body) => {
+
+        var resPrice = JSON.parse(body);
+
+        if (resPrice.text == 'no such id') {
+
+            res.render('index', {
+                layout: false,
+                title: "Your Item",
+                data: itemData,
+                message: "Current ID has no commerce data"
+            });
+
+        } else if (!err && res.statusCode == 200) {
+
+            itemData = Object.assign(itemData, {
+                buys: {
+                    quantity: resPrice.buys.quantity,
+                    price: resPrice.buys.unit_price
+                },
+                sells: {
+                    quantity: resPrice.sells.quantity,
+                    price: resPrice.sells.unit_price
+                }
+            });
+
+            res.render('index', {
+                layout: false,
+                title: "Your Searched Items",
+                data: itemData
+            });
+        }
+    });
+};
--- app/routes.js
+++ app/routes.js
@@ -2,13 +2,19 @@
 
 'use strict';
 
+var itemCtrl = require('./controllers/items.server.controller');
+
 module.exports = function(app) {
 
     app.get('/', (req, res) => {
-        res.render("index", {   
-           title: "GW2 Calculator",
-           text: "Hello World!" 
+        res.render('index', {
+            title: "GW2 Calculator",
         });
     });
 
+    /**
+     * Get item info from GW2 API
+     */
+    app.route('/api/item')
+        .get(itemCtrl.getItemData);
 };
