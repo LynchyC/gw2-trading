a53513795bab6b10291c7d663a8254fe7eb3f893
Lynchy <lynchy95charles@gmail.com>
2016-03-24 11:32:29 +0000

    Modified backend conditioning

--- app/controllers/items.server.controller.js
+++ app/controllers/items.server.controller.js
@@ -4,7 +4,6 @@
 
 var request = require('request');
 
-
 exports.getItemData = (req, res) => {
     let id = parseInt(req.query.itemID);
 
@@ -14,50 +13,51 @@ exports.getItemData = (req, res) => {
             title: "Error has occured",
             error: "Invalid ID. Please try again."
         });
-        
-        return;
+    } else {
+
+        let itemData = {
+            name: '',
+            img: '',
+            buys: {
+                quantity: null,
+                price: null
+            },
+            sells: {
+                quantity: null,
+                price: null
+            }
+        };
+
+        request(`https://api.guildwars2.com/v2/items/${id}`, (err, response, body) => {
+
+            var resBody = JSON.parse(body);
+
+            if (err) {
+                res.render('index', {
+                    layout: false,
+                    title: "Error has Occured",
+                    error: err
+                });
+            } else if (resBody.text == 'no such id') {
+                res.render('index', {
+                    layout: false,
+                    title: "Error has occured",
+                    error: "ID does not exist. Please check your ID."
+                });
+
+            } else if (!err && response.statusCode == 200) {
+
+                itemData = Object.assign(itemData, {
+                    name: resBody.name,
+                    img: resBody.icon
+                });
+
+                exports.getPriceData(res, id, itemData);
+            }
+        });
     }
 
-    let itemData = {
-        name: '',
-        img: '',
-        buys: {
-            quantity: null,
-            price: null
-        },
-        sells: {
-            quantity: null,
-            price: null
-        }
-    };
 
-    request(`https://api.guildwars2.com/v2/items/${id}`, (err, response, body) => {
-
-        var resBody = JSON.parse(body);
-
-        if (resBody.text == 'no such id') {
-            res.render('index', {
-                layout: false,
-                title: "Error has occured",
-                error: "ID does not exist. Please check your ID."
-            });            
-            
-        } else if (!err && res.statusCode == 200) {
-
-            itemData = Object.assign(itemData, {
-                name: resBody.name,
-                img: resBody.icon
-            });
-
-            exports.getPriceData(res, id, itemData);
-        } else {
-            res.render('index', {
-                layout: false,
-                title: "Error has Occured",
-                error: err
-            });            
-        }
-    });
 };
 
 exports.getPriceData = (res, id, itemData) => {
@@ -65,16 +65,20 @@ exports.getPriceData = (res, id, itemData) => {
 
         var resPrice = JSON.parse(body);
 
-        if (resPrice.text == 'no such id') {
-
+        if (err) {
+            res.render('index', {
+                layout: false,
+                title: "Error has Occured",
+                error: err
+            });
+        } else if (resPrice.text == 'no such id') {
             res.render('index', {
                 layout: false,
                 title: "Your Item",
                 data: itemData,
                 message: "Current ID has no commerce data"
-            });            
-
-        } else if (!err && res.statusCode == 200) {
+            });
+        } else if (!err && response.statusCode == 200) {
 
             itemData = Object.assign(itemData, {
                 buys: {
@@ -92,12 +96,6 @@ exports.getPriceData = (res, id, itemData) => {
                 title: "Your Searched Items",
                 data: itemData
             });
-        } else {
-            res.render('index', {
-                layout: false,
-                title: "Error has Occured",
-                error: err
-            });                        
-        }
+        } 
     });
 };
