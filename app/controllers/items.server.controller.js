4f6ec7cdc4d14058c4cd57a6449604a10c17bcce
git <charlie.lynch95@gmail.com>
2016-04-09 11:55:32 +0100

    Updated utilities. Added coin ratio function to display currency to user.

--- app/controllers/items.server.controller.js
+++ app/controllers/items.server.controller.js
@@ -2,7 +2,8 @@
 
 'use strict';
 
-var utils = require('./../utils/utils.js');
+var apiUtils = require('./../utils/apiUtils.js');
+var coinUtils = require('./../utils/coinUtils.js');
 
 /**
  * Function to build the data object that is sent back to the user
@@ -35,7 +36,7 @@ exports.getItemData = (id, next) => {
             }
         };
 
-        utils.gw2APIData(`items/`, itemData._id, (err, results) => {
+        apiUtils.gw2APIData(`items/`, itemData._id, (err, results) => {
             if (err) {
 
                 next({
@@ -66,18 +67,19 @@ exports.getItemData = (id, next) => {
 
 exports.getPriceData = (itemData, next) => {
 
-    utils.gw2APIData(`commerce/prices/`, itemData._id, (err, results) => {
+    apiUtils.gw2APIData(`commerce/prices/`, itemData._id, (err, results) => {
         if (err) {
             next(err, null);
         } else {
+
             itemData = Object.assign(itemData, {
                 buys: {
                     quantity: results.buys.quantity,
-                    price: results.buys.unit_price
+                    price: coinUtils.calucatePriceRatio(results.buys.unit_price)
                 },
                 sells: {
                     quantity: results.sells.quantity,
-                    price: results.sells.unit_price
+                    price: coinUtils.calucatePriceRatio(results.sells.unit_price) 
                 }
             });
 
---
+++ app/utils/coinUtils.js
@@ -0,0 +1,26 @@
+// app/utils/coinUtils.js
+
+'use strict';
+
+/**
+ * Calculates the Gold/Silver/Bronze ratio for item
+ */
+
+exports.calucatePriceRatio = (unit_price) => {
+    let gold = parseInt(unit_price / 10000);
+    let silver = 0;
+    if (gold > 0) {
+        silver = parseInt(unit_price % 10000);
+        if (silver >= 100) {
+            silver = parseInt(silver / 100);
+        }
+    } else {
+        silver = parseInt(unit_price / 100);
+    }
+
+    let bronze = parseInt(unit_price % 100);
+
+    let price = [gold, silver, bronze];
+
+    return price;
+};
