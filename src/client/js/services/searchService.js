2e9b512ef7fd08eb45cef1d7968c7a177bbba089
Charlie Lynch <lynchcoding@gmail.com>
2017-03-04 11:23:38 +0000

    Remove IIFE from searchAPI service.

--- src/client/js/services/searchService.js
+++ src/client/js/services/searchService.js
@@ -1,16 +1,11 @@
-(function() {
-
-    'use strict';
-
-    angular.module('gw2Calc').factory('searchAPI', ['$resource',
-        function($resource) {
-            return $resource('/api/item/:item', {
-                item: '@item'
-            }, {
-                query: {
-                    method: 'GET'
-                }
-            });
-        }
-    ]);
-}());
+angular.module('gw2Calc').factory('searchAPI', ['$resource',
+    function ($resource) {
+        return $resource('/api/item/:item', {
+            item: '@item'
+        }, {
+            query: {
+                method: 'GET'
+            }
+        });
+    }
+]);
