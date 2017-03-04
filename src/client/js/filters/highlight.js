a4d5a5bb98d1321ebbab7c978f69358663bb8c28
Charlie Lynch <lynchcoding@gmail.com>
2017-03-04 17:33:15 +0000

    Added highlight to matched search results

---
+++ src/client/js/filters/highlight.js
@@ -0,0 +1,11 @@
+angular.module('gw2Calc')
+    .filter('highlight', function ($sce) {
+        return function (text, phrase) {
+            if (phrase) {
+                text = text.replace(new RegExp('(' + phrase + ')', 'gi'),
+                    '<span class="highlighted">$1</span>');
+            }
+
+            return $sce.trustAsHtml(text);
+        };
+    });
